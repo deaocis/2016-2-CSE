ac53e827f0803c45ab4ce248d7696bae7a53e456
유동호 <deaocis@gmail.com>
2016-09-21 14:32:12 +0900

    complete Lab

--- GLFW-test/GLFW-test/source.cpp
+++ GLFW-test/GLFW-test/source.cpp
@@ -23,6 +23,71 @@ void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const 
 	}
 }
 
+void drawBox(const int& i_c, const int& j_c)
+{
+	const int thickness = 60;   //두께
+
+	for (int j = j_c - thickness; j < j_c + thickness; j++)
+		for (int i = i_c - thickness; i < i_c + thickness; i++)
+		{
+			drawPixel(i, j, 0.0f, 1.0f, 0.0f);
+		}
+}
+
+void drawTriangle(const int& left, const int& right, const int& up)
+{
+	drawLine(left, left, right, left, 1.0f, 0.0f, 1.0f);
+	drawLine(up, up, right, left, 1.0f, 0.0f, 1.0f);
+	drawLine(left, left, up, up, 1.0f, 0.0f, 1.0f);
+}
+
+void drawEmptyBox(const int& i_s, const int& j_s,const int& i_e, const int& j_e)  //s=start, e=end
+{
+	for (int j = j_s; j <= j_e; j++)
+		for (int i = i_s; i <= i_e; i++)
+		{
+			if(j==j_e||j==j_s||i==i_s||i==i_e)
+				drawPixel(i, j, 1.0f, 0.0f, 0.0f);
+			else 
+				drawPixel(i, j, 1.0f, 1.0f, 1.0f);
+		}
+}
+
+//void drawPentagon(const int& i0, const int& i1, const int& i2, const int& i3, const int& i4)  //No
+//{
+//	drawLine(i0, i0, i1, i1, 0.0f, 0.0f, 1.0f);
+//	drawLine(i1, i1, i2, i2, 0.0f, 0.0f, 1.0f);
+//	drawLine(i2, i2, i3, i3, 0.0f, 0.0f, 1.0f);
+//	drawLine(i3, i3, i4, i4, 0.0f, 0.0f, 1.0f);
+//	drawLine(i4, i4, i0, i0, 0.0f, 0.0f, 1.0f);
+//}
+
+void drawCircle(const int& x_c, const int& y_c,const int& radius)  //first - distance=radius ->draw
+{
+	for(double j=0;j<height;j++)
+		for (double i = 0; i < width; i++)
+		{
+			double func = ((i - x_c)*(i - x_c) + (j - y_c)*(j - y_c) - radius*radius);
+
+			if (func >= 0 && func <= 60)    //func>=0&&func<=9 , func == 0||func==1||func==2||func==5
+				drawPixel(i, j, 1.0f, 0.0f, 0.0f);
+			else
+				drawPixel(0, 0, 1.0f, 1.0f, 1.0f);
+		}
+}
+
+//void drawCircle(const int& x_c, const int y_c)
+//{
+//	const double x_c = 0.5;
+//	const double y_c = 0.5;
+//	const double r = 0.5;
+//
+//	const double func = (x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) - r*r;
+//
+//	if (func > 0.0) return false;
+//	else return true;
+//}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -49,7 +114,36 @@ int main(void)
 
 		std::fill_n(pixels, width*height * 3, 1.0f);	// white background
 
-		drawLine(0, 0, 639, 479, 1.0f,.0f, 0.0f);      //선 그릴수 있음.
+		
+		drawLine(50, 350, 150, 450, 0.0f, 0.0f, 1.0f);
+		drawLine(51, 350, 151, 450, 0.0f, 0.0f, 1.0f);
+		drawLine(52, 350, 152, 450, 0.0f, 0.0f, 1.0f);  //3-width-line
+
+		//for(int j=100;j<300;j++)
+		//	for (int i = 100; i < 300; i++)
+		//	{
+		//		pixels[(i + width*j) * 3 + 0] = 0.0f;
+		//		pixels[(i + width*j) * 3 + 1] = 1.0f;
+		//		pixels[(i + width*j) * 3 + 2] = 0.0f;
+		//	}
+
+		drawBox(550, 400);               //full box
+
+		drawEmptyBox(250,300,400, 450);   //i_s,j_s,i_e,j_e, empty box
+
+		drawTriangle(40, 160, 120);     //left,right,up  triangle
+
+		//drawLine(50, 150, 130, 229, 0.0f, 0.0f, 1.0f);
+		//drawLine(51, 150, 180, 150, 0.0f, 0.0f, 1.0f);
+		//drawLine(130, 229, 180, 150, 0.0f, 0.0f, 1.0f);  //triangle
+
+		drawLine(300, 100, 370, 50, 0.0f, 0.0f, 1.0f);
+		drawLine(230, 50, 300, 100, 0.0f, 0.0f, 1.0f);
+		drawLine(230, 50, 260, 10, 0.0f, 0.0f, 1.0f);
+		drawLine(260, 10, 340, 10, 0.0f, 0.0f, 1.0f);
+		drawLine(340, 10, 370, 50, 0.0f, 0.0f, 1.0f);   //pentagon
+
+		drawCircle(500,100,40);  //circle
 
 		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
 		//We do that.
@@ -67,3 +161,9 @@ int main(void)
 	glfwTerminate();
 	return 0;
 }
+
+//구현하고 싶은거: 원, 안에 빈 사각형, 안에 꽉찬 삼각형
+//아마 안에 빈 원은 원의 방정식=0 이면 될듯?
+
+//왼쪽밑이 (0,0)이니까..
+
